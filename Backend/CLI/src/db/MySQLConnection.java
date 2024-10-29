a8ea26baf33956974158ede64cdd6ad01abf9299
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-10-30 01:34:05 +0530

    create MySQLConnection

--- Backend/CLI/src/db/MySQLConnection.java
+++ Backend/CLI/src/db/MySQLConnection.java
@@ -1,4 +1,35 @@
 package src.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLConnection {
+    private static MySQLConnection instance;
+    private Connection connection;
+
+    private MySQLConnection() {
+        // Database credentials
+        String url = "jdbc:mysql://localhost:3306/event_ticketing_db";
+        String user = "root";
+        String password = "12345";
+
+        try {
+            this.connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Database connected successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        }
+    }
+
+    public static MySQLConnection getInstance() {
+        if (instance == null) {
+            instance = new MySQLConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
